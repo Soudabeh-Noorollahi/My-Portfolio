ac40ecd4f2b1b05ed3b4e98553ef24058eea6cbe
Soudabeh Noorollahi <s.nourollahii@gmail.com>
2025-05-30 11:19:10 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="min-h-screen bg-gradient-to-r from-pink-500 to-yellow-500 text-white flex items-center justify-center text-3xl font-bold">Portfolio</h1>
+    </>
+  );
+}
+
+export default App;
