2a5af531a37fb952719ae8c74650e6bd0140b704
Soudabeh Nourollahi <s.nourollahii@gmail.com>
2025-07-17 22:31:48 +0200

    chore: add project description for My Market in portfolio data

--- src/constants/project.js
+++ src/constants/project.js
@@ -1,41 +1,49 @@
 import BookApp from "../assets/projects/Book-App.png";
-import CarNest from "../assets/projects/CarNest.png";
+// import CarNest from "../assets/projects/CarNest.png";
 import ContactApp from "../assets/projects/ContactApp.png";
 import CRMApp from "../assets/projects/CRMApp.png";
 import CryptoApp from "../assets/projects/Crypto-App.png";
 import FoodJoy from "../assets/projects/FoodJoy.png";
 import StoreProject from "../assets/projects/Store-Project.png";
 import ToDoGo from "../assets/projects/ToDoGo.png";
+import mymarketapp from "../assets/projects/mymarketapp.JPG";
 
 const projects = [
   {
     id: "1",
-    name: "Todo App",
-    image: ToDoGo,
-    desc: "A full-stack Todo App built with Next.js, including task management, authentication, and a responsive UI",
-    url: "https://github.com/Soudabeh-Noorollahi/Todo-App.git",
+    name: "My Market",
+    image: mymarketapp,
+    desc: "A responsive online classifieds platform built with React, TailwindCSS, and Node.js. Users can post ads, browse categories, and manage their listings with authentication and role-based access. Optimized for mobile and desktop.",
+    url: "https://github.com/Soudabeh-Noorollahi/my-market-app.git",
   },
   {
     id: "2",
+    name: "Todo App",
+    image: ToDoGo,
+    desc: "A full-stack Todo App built with Next.js, including task management, authentication, and a responsive UI.",
+    url: "https://github.com/Soudabeh-Noorollahi/Todo-App.git",
+  },
+  {
+    id: "3",
     name: "CRM App",
     image: CRMApp,
     desc: "A fullstack CRM-App using Next.js and MongoDB.",
     url: "https://github.com/Soudabeh-Noorollahi/CRM-App.git",
   },
   {
-    id: "3",
+    id: "4",
     name: "Food Joy",
     image: FoodJoy,
     desc: "A recipe website built with Next.js, providing a modern and user-friendly platform for discovering and sharing delicious recipes.",
     url: "https://github.com/Soudabeh-Noorollahi/Food-Joy.git",
   },
-  {
-    id: "4",
-    name: "Car-Nest Project ",
-    image: CarNest,
-    desc: "A Next.js project for creating dynamic car dealership website with advanced search and car listing features.",
-    url: "https://github.com/Soudabeh-Noorollahi/Car-Nest-Project.git",
-  },
+  // {
+  //   id: "4",
+  //   name: "Car-Nest Project ",
+  //   image: CarNest,
+  //   desc: "A Next.js project for creating dynamic car dealership website with advanced search and car listing features.",
+  //   url: "https://github.com/Soudabeh-Noorollahi/Car-Nest-Project.git",
+  // },
   {
     id: "5",
     name: "Store App",
